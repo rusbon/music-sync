fdf77ba8670a9e0c95fbbe2507773a8ef2da073b
rusbon <sulaimanakbar29@gmail.com>
2024-10-14 05:16:09 +0700

    fix: add build script

---
+++ build.sh
@@ -0,0 +1,7 @@
+bash -c 'cd server && npm ci'
+bash -c 'cd client && yarn install --frozen-lockfile'
+
+bash -c 'cd server && tsc'
+bash -c 'cd server && npx prisma migrate deploy'
+cp -r server/node_modules dist/
+bash -c 'cd client && npm run build'
